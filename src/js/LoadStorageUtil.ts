e3835ec694e2741c39cac05e1a5d126ae17372c3
Emma Larsson <emma940112@gmail.com>
2024-04-20 06:39:09 +0200

    Skapat en klass för att spara och hämta information i localStorage

---
+++ src/js/LoadStorageUtil.ts
@@ -0,0 +1,20 @@
+import { Task } from "./ToDo";
+
+export class LocalStorageUtil {
+    static saveTasks(tasks: Task[]) {
+        //Sparar task-arrayen till localStorage
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+    //Hämtar task-arrayen från localStorage
+    static loadTasks(): Task[] {
+        const taskStr = localStorage.getItem("tasks");
+
+        if (taskStr) {
+            return JSON.parse(taskStr);
+        } else {
+            //Om inga uppgifter finns sparade, returnera en tom array
+            return [];
+        }
+    }
+}
